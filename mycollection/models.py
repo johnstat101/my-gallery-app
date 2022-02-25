28354488a959a15fd2ebdf8d24b833c75223dd9d
johnstat101 <johnjorogek@gmail.com>
2022-02-25 14:37:19 +0300

    add image models

--- mycollection/models.py
+++ mycollection/models.py
@@ -1,3 +1,21 @@
+from email.mime import image
+from unicodedata import category
 from django.db import models
 
-# Create your models here.
+# Category Model
+class Category(models.Model):
+    image_category = models.CharField(max_length=30)
+
+# Location Model
+class Location(models.Model):
+    image_location = models.CharField(max_length=30)
+
+
+# image model
+class Image(models.Model):
+    image = models.ImageField(upload_to = "images/")
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField(max_length=100)
+    image_location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
