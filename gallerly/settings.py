e8bb0a490de026a6d5a8bcf0435c77cf76490229
johnstat101 <johnjorogek@gmail.com>
2022-02-25 13:20:53 +0300

    add install bootstrap and add to settings

--- gallerly/settings.py
+++ gallerly/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
+    'mycollection'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
